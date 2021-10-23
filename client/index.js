05837ebd859fb789245bea219f545e2320b0b93a
Laurence Marrin <Laurencemarrin@gmail.com>
2021-10-23 16:55:08 -0600

    real elliptic keys & signature verification

--- client/index.js
+++ client/index.js
@@ -1,27 +1,35 @@
 import "./index.scss";
 
 const server = "http://localhost:3042";
-
+let privateKey;
 document.getElementById("exchange-address").addEventListener('input', ({ target: {value} }) => {
-  if(value === "") {
+  if (value === "") {
     document.getElementById("balance").innerHTML = 0;
     return;
   }
 
   fetch(`${server}/balance/${value}`).then((response) => {
     return response.json();
-  }).then(({ balance }) => {
+  }).then(({ balance, pkey }) => {
+    privateKey = pkey;//
+    console.log({privateKey:privateKey})
     document.getElementById("balance").innerHTML = balance;
   });
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
+  const EC = require('elliptic').ec;
+  const ec = new EC('secp256k1');
+  const SHA256 = require('crypto-js/sha256');
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
-
+  const key = ec.keyFromPrivate(privateKey);
+  const txmsg = sender + "" + amount + "" + recipient;
+  const msgHash = SHA256(txmsg);
+  const signature = key.sign(msgHash.toString());
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, signature
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
--- server/index.js
+++ server/index.js
@@ -1,32 +1,89 @@
+//node index to run this one
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+const SHA256 = require('crypto-js/sha256');
+let GOD_ADDRESS = "";
 const port = 3042;
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
-
-const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+const balances = {}
+for (let i = 0; i < 4; i++) {
+  const key = ec.genKeyPair();
+  let pubkey = key.getPublic().encode('hex').substring(0,12)
+  balances[pubkey] = {
+    public: key.getPublic().encode('hex'),
+    private: key.getPrivate().toString(16),
+    balance: i*10+17
+  }
+  if (i == 3) {
+    GOD_ADDRESS = pubkey
+  }
 }
+// const publicKey = key.getPublic().encode('hex');
+// console.log({
+//   privateKey: key.getPrivate().toString(16),
+//   publicX: key.getPublic().x.toString(16),
+//   publicY: key.getPublic().y.toString(16),
+// });
+// const balances = {
+//   "1": 100,
+//   "2": 50,
+//   "3": 75,
+// }
 
 app.get('/balance/:address', (req, res) => {
-  const {address} = req.params;
-  const balance = balances[address] || 0;
-  res.send({ balance });
+  const { address } = req.params;
+  console.log({address: address})
+  console.log({ balances: balances })
+  let balance = 0
+  let pkey = ""
+  if (balances[address]) {
+    console.log({balance_for_address:balances[address]})
+    balance = balances[address]["balance"] || 0;
+    pkey = balances[address]["private"];
+  }
+  res.send({ balance, pkey });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const { sender, recipient, amount, signature } = req.body;
+  console.log({sender:sender})
+  // console.log(balances)
+  let publicKey = balances[sender]["public"]
+  // console.log({publicKey:publicKey})
+  const key = ec.keyFromPublic(publicKey, 'hex');
+  const txmsg = sender + "" + amount + "" + recipient;
+  const msgHash = SHA256(txmsg);
+  console.log({msgHash:msgHash, signature:signature})
+  const verified = key.verify(msgHash, signature)
+  // const verified = true
+  console.log({ verification: verified });
+  if (!verified) {
+    console.log("UNVERIFIED TRANSACTION, SENDERS FUNDS ARE BEING CONFISCATED")
+    confiscateFunds(sender)
+  } else if (amount > balances[sender]["balance"]) {
+    console.log("SENT MORE THAN YOU HAVE, SENDERS FUNDS ARE BEING CONFISCATED")
+    confiscateFunds(sender)
+  } else {
+    balances[sender]["balance"] -= amount;
+    balances[recipient]["balance"]  = (balances[recipient]["balance"]  || 0) +amount;
+  }
+  console.log({new_sender_balance:balances[sender]});
+  res.send({ balance: balances[sender]["balance"] });
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}!`);
+  console.log(balances)
 });
+
+function confiscateFunds(address) {
+   balances[GOD_ADDRESS]["balance"] = (balances[GOD_ADDRESS]["balance"] || 0) + balances[address]["balance"];
+   balances[address] = 0;
+}
